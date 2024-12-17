5c131924bae5e672191a76b842e64276563ec93b
Connor Engel <cengel@lcs.on.ca>
2024-12-17 09:03:48 -0500

    Initial Commit

---
+++ HxH_HobbyCards/HxH_HobbyCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HxH_HobbyCardsApp.swift
+//  HxH_HobbyCards
+//
+//  Created by Connor Engel on 2024-12-17.
+//
+
+import SwiftUI
+
+@main
+struct HxH_HobbyCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
